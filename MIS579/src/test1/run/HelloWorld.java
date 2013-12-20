641818c77d53b9dad3ddc41ec400a0594fd324de
Matthew Wolff <wolffml@yahoo.com>
2013-12-20 10:05:41 -0600

    Initial

---
+++ MIS579/src/test1/run/HelloWorld.java
@@ -0,0 +1,19 @@
+/* Copyright 2013 Caterpillar Inc. All rights reserved. 
+ * The work contains Caterpillar’s proprietary information, 
+ * which may constitute a trade secret and/or be confidential. 
+ * Copyright notice is precautionary only and does not imply publication.
+ * 
+ *  
+ * Create By : wolffml
+ * Create On : Dec 20, 2013
+ */
+package test1.run;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
+
