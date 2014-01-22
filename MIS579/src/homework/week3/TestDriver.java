c9e554693868123e25515f7893262cf4d9788f1d
Matthew Wolff <wolffml@yahoo.com>
2014-01-22 15:50:54 -0600

    Some updates to TestDriver

--- MIS579/src/homework/week3/TestDriver.java
+++ MIS579/src/homework/week3/TestDriver.java
@@ -1,5 +1,7 @@
 package homework.week3;
 
+import java.util.Scanner;
+
 /*
 * Matt Wolff
 * MIS579 iLab / Week 3 / Problem 2
@@ -17,6 +19,13 @@ package homework.week3;
 
 public class TestDriver {
 	
+	public static MenuOption[] MENU_OPTIONS = { 
+		new MenuOption("Rectangle",'R',"test"),
+		new MenuOption("Square", 'S', "test2"),
+		new MenuOption("Help", 'H', "help"),
+		new MenuOption("Exit", 'E', "exit")
+	};
+	
 	
 	public static void main(String[] args){
 		
@@ -32,5 +41,64 @@ public class TestDriver {
 			
 		} while (blnContinue);
 	}
+	
+	public static String getValidatedInput(){
+		Scanner sc = new Scanner(System.in);
+		while (!sc.hasNext(getRegEx())) {
+		        System.out.println("Invalid Choice please choose any of the following");
+		        sc.next();
+		}
+		return "";
+	}
+	
+	public static String getRegEx(){
+		String strReturn = "";
+		String characters = "";
+		for (MenuOption menu: MENU_OPTIONS){
+			characters += menu.getSymbol();
+		}
+		strReturn = "[" + characters.toUpperCase() + characters.toLowerCase() + "]";
+		return strReturn;
+	}
+	
+	private String getAvailableMenuCommands(){
+		String strReturn = "";
+		for (MenuOption menu: MENU_OPTIONS){
+			strReturn += "(" + ")" + menu.getSymbol() + " " + menu.getName() + "\n"; 
+		}
+		return strReturn;
+	}
+	
+	private static class MenuOption{
+		
+		private String name;
+		private char symbol;
+		private String action;
+		public MenuOption(String name, char symbol, String action) {
+			this.name = name;
+			this.symbol = symbol;
+			this.action = action;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public char getSymbol() {
+			return symbol;
+		}
+		public void setSymbol(char symbol) {
+			this.symbol = symbol;
+		}
+		public String getAction() {
+			return action;
+		}
+		public void setAction(String action) {
+			this.action = action;
+		}
+		
+	}
+	
 }
 
