5719146a921bfa5421cf5636cc1d35c144b31043
wolffml <wolffml@yahoo.com>
2014-01-30 11:21:35 -0600

    Started Week 4

---
+++ MIS579/src/homework/week4/Demo.java
@@ -0,0 +1,14 @@
+package homework.week4;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void giveExceptionA() throws ExceptionA{
+		throw new ExceptionA();
+	}
+
+}
